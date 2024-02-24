98b6bd6d4b6fe24bca5a7151ab7a69ad12a568f4
Federico Balducci <ott8v@proton.me>
2024-02-24 17:22:58 +0100

    complete connection to db

--- model/index.js
+++ model/index.js
@@ -1,6 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config()
 
-const sequelize = new Sequelize('database', 'username', 'pass', {
-    host: 'localhost',
+console.log(process.env.DB_HOST)
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PSSW, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
 });
+
+try {
+    await sequelize.authenticate();
+} catch (e) {
+    console.error(e);
+}
