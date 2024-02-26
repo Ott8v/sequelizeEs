2fb32a098a3abe9d236594e3f7957b23b902ba7a
Federico Balducci <ott8v@proton.me>
2024-02-26 16:33:26 +0100

    fix error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 db.sequelize.sync();
 
-require('./app/routes/patient.route.js');
+require('./app/routes/patient.route.js')(app);
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000")
